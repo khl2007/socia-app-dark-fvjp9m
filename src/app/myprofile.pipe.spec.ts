5020dc899d1c6dd2afcc3c590ec6c5a5d4012183
khl2007 <khalilqandil@gmail.com>
2020-04-21 06:58:17 -0700

    6 files deleted

--- src/app/myprofile.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MyprofilePipe } from './myprofile.pipe';
-
-describe('MyprofilePipe', () => {
-  it('create an instance', () => {
-    const pipe = new MyprofilePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
