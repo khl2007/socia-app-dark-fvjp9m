e7cad230da1fd57f72f2999e9a47876d8b3444e8
khl2007 <khalilqandil@gmail.com>
2020-04-22 03:22:41 +0300

    Update myprofile.page.ts

--- src/app/myprofile/myprofile.page.ts
+++ src/app/myprofile/myprofile.page.ts
@@ -1,4 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { Observable } from "rxjs";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { auth } from "firebase/app";
+import {
+  ToastController,
+  AlertController
+} from "@ionic/angular";
+import { ImagePicker } from "@ionic-native/image-picker/ngx";
+import { WebView } from "@ionic-native/ionic-webview/ngx";
+import { User } from "../services/user";
+import { UserfowlowComponent } from '../comps/userfowlow/userfowlow.component';
+import { UserfolowersComponent } from '../comps/userfolowers/userfolowers.component';
+import { flatMap, map } from "rxjs/operators";
+       
+import { FirebaseService } from "../services/firebase.service";
+//import { Animation, AnimationController } from '@ionic/angular';
+import { FollowService } from "../services/follow.service";
+import { AuthService } from "../services/auth.service";
+import { LoadingController } from "@ionic/angular";
+import { Router, ActivatedRoute } from "@angular/router";
+import { IonRouterOutlet } from '@ionic/angular';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-myprofile',
