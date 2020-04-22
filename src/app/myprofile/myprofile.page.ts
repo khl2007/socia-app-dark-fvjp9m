bb8ff17f1c55c83731eacefd8419b5b7c298c197
khl2007 <khalilqandil@gmail.com>
2020-04-22 03:48:12 +0300

    Update myprofile.page.ts

--- src/app/myprofile/myprofile.page.ts
+++ src/app/myprofile/myprofile.page.ts
@@ -60,4 +60,71 @@ getuserdata(userid) {
    // console.log(this.userData);
   }
 
+updateUserdata(){
+
+let data = {
+      displayName : this.displayName;
+      imgurl: this.useravtar,
+    };
+
+this.firebaseService.updateuserdata(userId,data);
+
+}
+
+changepic(){
+this.imagePicker.hasReadPermission().then(
+      result => {
+        if (result == false) {
+          // no callbacks required as this opens a popup which returns async
+          this.imagePicker.requestReadPermission();
+        } else if (result == true) {
+          this.imagePicker
+            .getPictures({
+              maximumImagesCount: 1
+            })
+            .then(
+              results => {
+                for (var i = 0; i < results.length; i++) {
+                  this.uploadImageToFirebase(results[i]);
+                }
+              },
+              err => console.log(err)
+            );
+        }
+      },
+      err => {
+        console.log(err);
+      }
+    );
+
+}
+async uploadImageToFirebase(image) {
+    const loading = await this.loadingCtrl.create({
+      message: "Please wait..."
+    });
+    const toast = await this.toastCtrl.create({
+      message: "Image was updated successfully",
+      duration: 3000
+    });
+    this.presentLoading(loading);
+    // let image_to_convert = 'http://localhost:8080/_file_' + image;
+    let image_src = this.webview.convertFileSrc(image);
+    let randomId = Math.random()
+      .toString(36)
+      .substr(2, 5);
+
+    //uploads img to firebase storage
+    this.firebaseService.uploadImage(image_src, randomId).then(
+      photoURL => {
+        this.useravtar = photoURL;
+        this.firebaseService.updateuseravatar(this.curentuserid,photoURL);
+        loading.dismiss();
+        toast.present();
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
 }
